261f6044d72b195486a860d49df1f590a2e0fd0b
JuanDavidBallesteros <juan.ballesteros@u.icesi.edu.co>
2021-03-12 12:24:14 -0500

    read options

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,81 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
 public class Main {
+
+    private static List<String> lines;
+    private static List<Integer> prices;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        prices = new ArrayList<>();
+        lines = new ArrayList<>();
+
+        String line = br.readLine();
+        Boolean out = false;
+
+        while (line != null && !out) {         
+            lines.add(line);
+            line = br.readLine();     
+            if(line.equals("")) {
+                out = true;
+            }
+        }
+
+        String[] parts = lines.get(1).split(" ");
+        for(int i = 0 ; i < parts.length ; i++){
+            prices.add(Integer.parseInt(parts[i]));
+        }
+
+        br.close();
+
+
+        
+        bw.write(result(Integer.parseInt(lines.get(2)), prices));
+        bw.close();
+    }
+
+    public static String result (int amount, List<Integer> values){
+        String ms = "no values";
+
+        for (int i = 0; i < values.size() ; i++){
+
+            int testValue = values.get(i);
+            int subtract = 1000001;
+            
+
+            for(int j = i+1; j < values.size(); j++){
+                if(testValue + values.get(j) == amount && testValue < j){
+
+                    if(values.get(j) - testValue <= subtract){
+                        ms = testValue + " , " + values.get(j);
+                        subtract = values.get(j) - testValue;
+                        System.out.println(subtract + " = " + values.get(j) + " - " + testValue);
+                    }   
+                }
+                
+                if(testValue + values.get(j) == amount && testValue >= j){
+                    
+                    if(testValue - values.get(j) <= subtract){
+                        ms = values.get(j) + " , " + testValue ;
+                        subtract =  testValue - values.get(j);
+                        System.out.println(subtract + " = " + testValue+ " - " + values.get(j));
+                    }   
+                }
+            }
+        }
+            
+        return ms;
+
     }
 }
