e51ee8c9f9bf0471a118fa582233f062ff3ed162
JuanDavidBallesteros <juan.ballesteros@u.icesi.edu.co>
2021-03-12 15:54:03 -0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,78 @@
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 
 public class Main {
-
-    private static List<String> lines;
     private static List<Integer> prices;
-
-    public static void main(String[] args) throws Exception {
-        
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         prices = new ArrayList<>();
-        lines = new ArrayList<>();
 
-        String line = br.readLine();
-        Boolean out = false;
-
-        while (line != null && !out) {         
-            lines.add(line);
-            line = br.readLine();     
-            if(line.equals("")) {
-                out = true;
+        String line;
+        do{
+            line = br.readLine();
+            line = br.readLine();
+            String[] parts = line.split(" ");
+            for(int i = 0 ; i < parts.length ; i++){
+                prices.add(Integer.parseInt(parts[i]));
             }
-        }
+            line = br.readLine();
+            int amount = Integer.parseInt(line);
 
-        String[] parts = lines.get(1).split(" ");
-        for(int i = 0 ; i < parts.length ; i++){
-            prices.add(Integer.parseInt(parts[i]));
-        }
+            
+           String out = printResult(result(amount, prices));
 
+            bw.write(out);
+
+            line = br.readLine();
+        }while(line != null);
         br.close();
-
-
-        
-        bw.write(result(Integer.parseInt(lines.get(2)), prices));
         bw.close();
     }
 
-    public static String result (int amount, List<Integer> values){
+    public static String printResult(String ms) throws IOException{
+        
+        return ms = ("Peter should buy books whose prices are " + ms + "\n\n");
+    
+    }
+
+    public static String result(int amount, List<Integer> values) {
         String ms = "no values";
         int subtract = 1000001;
 
-        for (int i = 0; i < values.size() ; i++){
+        for (int i = 0; i < values.size(); i++) {
 
             int testValue = values.get(i);
-            
-            for(int j = i+1; j < values.size(); j++){
 
-                if(testValue + values.get(j) == amount){
+            for (int j = i + 1; j < values.size(); j++) {
 
-                    int temp =  testValue - values.get(j);
+                if (testValue + values.get(j) == amount) {
 
-                    if( temp >= 0 && temp < subtract ){ 
-                        
-                        ms = values.get(j) + " , " + testValue;
+                    int temp = testValue - values.get(j);
+
+                    if (temp >= 0 && temp < subtract) {
+
+                        ms = values.get(j) + " and " + testValue;
                         subtract = temp;
-                        System.out.println(subtract + " = " + values.get(j) + " - " + testValue);
 
-                    } else if( temp  < 0 &&  temp*-1  < subtract ){
+                    } else if (temp < 0 && temp * -1 < subtract) {
 
-                        ms =  testValue + " , " + values.get(j) ;
-                        subtract =  temp*-1;
-                        System.out.println(subtract + " = " + testValue+ " - " + values.get(j));
+                        ms = testValue + " and " + values.get(j);
+                        subtract = temp * -1;
                     }
                 }
-            } 
+            }
         }
-            
+
         return ms;
 
     }
