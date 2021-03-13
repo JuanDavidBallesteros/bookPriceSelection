5d723973fff3fffee923adf7b5a497d71b45fc35
JuanDavidBallesteros <juan.ballesteros@u.icesi.edu.co>
2021-03-13 12:00:20 -0500

    binarySearch corrected

--- src/Main.java
+++ src/Main.java
@@ -43,6 +43,52 @@ public class Main {
 
     }
 
+    public static String binarySearch(int amount, List<Integer> values) {
+
+        Collections.sort(values);
+
+        String out = "";
+        int subtract = 1000001;
+
+        for (int i = 0; i < (values.size()); i++) {
+            int x = amount - values.get(i);
+
+            int pos = -1;
+            int start = 0;
+            int last = values.size() - 1;
+
+            while (start <= last && pos < 0) {
+
+                int m = (start + last) / 2;
+
+                if (values.get(m) == (x)) {
+                    pos = m;
+
+                    int temp = values.get(i) - x;
+
+                    if (temp < 0) {
+                        temp = temp * -1;
+                    }
+
+                    if (temp < subtract) {
+                        subtract = temp;
+                        if (values.get(i) <= x) {
+                            out = values.get(i) + " and " + x;
+                        } else {
+                            out = x + " and " + values.get(i);
+                        }
+                    }
+
+                } else if (x > values.get(m)) {
+                    start = m + 1;
+                } else if (x < values.get(m)) {
+                    last = m - 1;
+                }
+            }
+        }
+        return out;
+    }
+
     public static String result(int amount, List<Integer> values) {
         String ms = "no values";
         int subtract = 1000001;
@@ -75,73 +121,4 @@ public class Main {
 
     }
 
-    public static String binarySearch(int amount, List<Integer> values) {
-
-        Collections.sort(values);
-
-        String out = "";
-        int subtract = 1000001;
-        int pos = -1;
-
-        int start = 0;
-        int last = values.size() - 1;
-
-        for (int i = 0; i < (values.size()/2) +1; i++) {
-            int x = amount - values.get(i);
-
-            while (start <= last && pos < 0) {
-
-                int m = (start + last) / 2;
-
-                if (values.get(m) == (x)) {
-                    pos = m;
-                } else if (x > values.get(m)) {
-                    start = m + 1;
-                } else if (x < values.get(m)) {
-                    last = m - 1;
-                }
-
-                int temp = values.get(i) - x;
-                if(temp < 0){
-                    temp = temp*-1;
-                }
-
-                if (temp < subtract && pos != -1) {
-
-                    if (values.get(i) < x) {
-                        out = values.get(i) + " and " + x;
-                        subtract = temp;
-                    } else {
-                        out = x + " and " + values.get(i);
-                        subtract = temp;
-                    }
-                }
-            }
-
-        }
-
-        /*
-         * for(int o = 0; o < values.size(); o++ ) {
-         * 
-         * i = o; j = values.size()-1;
-         * 
-         * int m = (o+j)/2;
-         * 
-         * if(values.get(o) + values.get(m) != amount) {
-         * 
-         * while(i < j && values.get(o) + values.get(m) != amount) { m = (i+j)/2;
-         * 
-         * if(values.get(o)+ values.get(m) == amount) { if(values.get(m)-values.get(o) <
-         * subtract) { subtract = values.get(m)-values.get(o); first = values.get(o);
-         * second = values.get(m); } } else if(values.get(o) + values.get(m) > amount) {
-         * j = m - 1; } else { i = m + 1; } }
-         * 
-         * } else { subtract = values.get(m)-values.get(o); first = values.get(o);
-         * second = values.get(m); }
-         * 
-         * }
-         */
-
-        return out;
-    }
 }
